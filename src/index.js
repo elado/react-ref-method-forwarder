19ab16b6df20809255cb68baf02b5018ed5bd083
Elad Ossadon <elad@ossadon.com>
2018-03-16 15:03:10 -0700

    initial code

--- src/index.js
+++ src/index.js
@@ -5,22 +5,63 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+const CONTEXT_TYPES = {
+  subscribeToForwardMethods: PropTypes.func.isRequired,
+};
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export const forwardMethodsOuter = ({ methods }) => WrappedComponent => {
+  class WithForwardedMethodsOuter extends React.Component {
+    callbacksByMethod = {};
+
+    static childContextTypes = CONTEXT_TYPES;
+
+    getChildContext() {
+      return {
+        subscribeToForwardMethods: this.subscribe,
+      };
+    }
+
+    subscribe = (innerRef) => {
+      methods.forEach(method => {
+        this.callbacksByMethod[method] = (...args) => {
+          return innerRef[method](...args);
+        };
+      });
+      return () => {
+        this.callbacksByMethod = {}
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  methods.forEach(method => {
+    WithForwardedMethodsOuter.prototype[method] = function(...args) {
+      return this.callbacksByMethod[method](...args);
+    };
+  });
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  return WithForwardedMethodsOuter;
+};
+
+export const forwardMethodsInner = () => WrappedComponent => {
+  class WithForwardedMethodsInner extends React.Component {
+    static contextTypes = CONTEXT_TYPES;
+
+    componentDidMount() {
+      this.unsubscribe = this.context.subscribeToForwardMethods(this.ref)
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+
+    render() {
+      return <WrappedComponent ref={ref => (this.ref = ref)} {...this.props} />;
+    }
   }
-}
+
+  return WithForwardedMethodsInner;
+};
